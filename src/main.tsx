19f33b153891f0586d058837f79c03004e66c970
emmanuel56mixta-ctrl <emmanuel56mixta@gmail.com>
2026-08-23 07:53:44 -0600

    Preview v0.10 React entry

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { Beatris } from "@/components/game/Beatris";
+import { BeatrisApp } from "@/components/beatris/BeatrisApp";
 import "@/styles.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Beatris />
+    <BeatrisApp />
   </StrictMode>,
 );
