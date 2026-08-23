c934c7d0efb41c50ca2bad3e7ee0958e1a18d418
emmanuel56mixta-ctrl <emmanuel56mixta@gmail.com>
2026-08-23 13:20:13 -0600

    v0.30: load quarry styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { AudioLabQuarry } from "@/components/lab/AudioLabQuarry";
 import "@/lab.css";
+import "@/quarry.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
