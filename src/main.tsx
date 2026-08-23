96dc14d572ede114abe226ff99fba392a3e5b151
emmanuel56mixta-ctrl <emmanuel56mixta@gmail.com>
2026-08-23 14:13:01 -0600

    Switch preview to v0.30.8 mixer console

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { AudioLabBuilderV307 } from "@/components/lab/AudioLabBuilderV307";
-import "@/console-v305.css";
-import "@/console-v306.css";
+import { AudioLabBuilderV308 } from "@/components/lab/AudioLabBuilderV308";
 import "@/console-v307.css";
+import "@/console-v308.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <AudioLabBuilderV307 />
+    <AudioLabBuilderV308 />
   </StrictMode>,
 );
