bfcfa08cd5b7df7d8d13ead8aedb0299358d8a31
emmanuel56mixta-ctrl <emmanuel56mixta@gmail.com>
2026-08-23 13:59:30 -0600

    Switch preview to v0.30.6 boosted live FX and 20 Fingers vocal

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { AudioLabBuilderV305 } from "@/components/lab/AudioLabBuilderV305";
+import { AudioLabBuilderV306 } from "@/components/lab/AudioLabBuilderV306";
 import "@/console-v305.css";
+import "@/console-v306.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <AudioLabBuilderV305 />
+    <AudioLabBuilderV306 />
   </StrictMode>,
 );
