133f065b540b76ee1728acb09ff5a98ad88b7fb5
emmanuel56mixta-ctrl <emmanuel56mixta@gmail.com>
2026-08-23 11:28:47 -0600

    Load v0.20.2 flow styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Beatris } from "@/components/game/Beatris";
 import "@/styles.css";
+import "@/flow.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
