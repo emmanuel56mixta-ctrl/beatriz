2130b0609db3056efc97065152a1ffb0c9378034
emmanuel56mixta-ctrl <emmanuel56mixta@gmail.com>
2026-08-23 13:47:55 -0600

    Switch preview to v0.30.4 transition FX lab

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { AudioLabBuilderV303 } from "@/components/lab/AudioLabBuilderV303";
+import { AudioLabBuilderV304 } from "@/components/lab/AudioLabBuilderV304";
 import "@/builder.css";
 import "@/builder-v303.css";
+import "@/builder-v304.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <AudioLabBuilderV303 />
+    <AudioLabBuilderV304 />
   </StrictMode>,
 );
