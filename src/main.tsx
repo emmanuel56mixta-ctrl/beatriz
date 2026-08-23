528cdab39f16a96f8a61902df9f158e671772952
emmanuel56mixta-ctrl <emmanuel56mixta@gmail.com>
2026-08-23 13:31:01 -0600

    Route v0.30 preview to quantized layer builder

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { AudioLabQuarry } from "@/components/lab/AudioLabQuarry";
-import "@/lab.css";
-import "@/quarry.css";
+import { AudioLabBuilder } from "@/components/lab/AudioLabBuilder";
+import "@/builder.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <AudioLabQuarry />
+    <AudioLabBuilder />
   </StrictMode>,
 );
