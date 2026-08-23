643822e371314b2f43402a202ffceb4493402cd7
emmanuel56mixta-ctrl <emmanuel56mixta@gmail.com>
2026-08-23 13:42:06 -0600

    Switch preview to v0.30.3 live instruments builder

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { AudioLabBuilder } from "@/components/lab/AudioLabBuilder";
+import { AudioLabBuilderV303 } from "@/components/lab/AudioLabBuilderV303";
 import "@/builder.css";
+import "@/builder-v303.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <AudioLabBuilder />
+    <AudioLabBuilderV303 />
   </StrictMode>,
 );
