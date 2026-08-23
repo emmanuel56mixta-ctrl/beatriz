331287269dcb9592a2a838ee8ea082a71cd26d48
emmanuel56mixta-ctrl <emmanuel56mixta@gmail.com>
2026-08-23 14:23:43 -0600

    Load v0.30.9 production mixer polish

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { AudioLabBuilderV308 } from "@/components/lab/AudioLabBuilderV308";
 import "@/console-v307.css";
 import "@/console-v308.css";
+import "@/console-v309.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
